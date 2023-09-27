176e9bbaae57d40121392d58311e5934313bb143
Ambigeswari67R <112757705+Ambigeswari67R@users.noreply.github.com>
2023-09-28 03:35:20 +0530

    Delete Backend/companyfacingutilitybillingapp directory

--- Backend/companyfacingutilitybillingapp/src/main/java/com/finzly/bharatbijili/CsvFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.finzly.bharatbijili;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CsvFileRepository extends JpaRepository<CsvFile, Integer>{
-
-}
